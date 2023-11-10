0df034cc44f4dbf706e9b2b9dc8a63934ab2a0db
mgildenberg <pulsesync@gmail.com>
2023-11-09 23:24:42 -0700

    add wrapper to form

--- src/App.js
+++ src/App.js
@@ -11,7 +11,31 @@ function App() {
       <Header />
       <Main weatherTemp={weatherTemp} />
       <Footer />
-      <ModalWithForm title="New Garment">These are the children</ModalWithForm>
+      <ModalWithForm title="New Garment">
+        <label>
+          Name
+          <input type="text" name="name" minlength="1" maxlength="30"></input>
+        </label>
+        <label>
+          Image URL
+          <input type="url" name="link" minlength="1"></input>
+        </label>
+        <p>Select the weather type:</p>
+        <div>
+          <div>
+            <input type="radio" id="hot" value="hot"></input>
+            <label>Hot</label>
+          </div>
+          <div>
+            <input type="radio" id="warm" value="warm"></input>
+            <label>Warm</label>
+          </div>
+          <div>
+            <input type="radio" id="cold" value="cold"></input>
+            <label>Cold</label>
+          </div>
+        </div>
+      </ModalWithForm>
     </div>
   );
 }
