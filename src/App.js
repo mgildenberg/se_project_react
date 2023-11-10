a2d092c98bae98a1b736f93c870ecae528c566ed
mgildenberg <pulsesync@gmail.com>
2023-11-09 23:18:35 -0700

    add modalwithform

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./footer/Footer";
+import ModalWithForm from "./ModalWithForm/ModalWithForm";
 
 function App() {
   const weatherTemp = "102°F"; // this will be replaced
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Main weatherTemp={weatherTemp} />
       <Footer />
+      <ModalWithForm title="New Garment">These are the children</ModalWithForm>
     </div>
   );
 }
